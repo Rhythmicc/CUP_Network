22ccba42a940a2c73e9c3ff6eeaa8618a354488d
Rhythmicc <rhythmlian.cn@gmail.com>
2023-05-10 11:34:27 +0800

    update

---
+++ build/lib/CUP_Network/__config__.py
@@ -0,0 +1,66 @@
+import os
+import json
+import base64
+from QuickProject import user_root, _ask, QproDefaultConsole, QproInfoString
+
+config_path = os.path.join(user_root, ".cup-network")
+
+questions = {
+    "username": {
+        "type": "input",
+        "message": "请输入校园网账号",
+    },
+    "password": {
+        "type": "password",
+        "message": "请输入校园网密码",
+    },
+    "docker-url": {
+        "type": "input",
+        "message": "远程selenuim服务器地址",
+    },
+}
+
+
+def encode_password(password):
+    return base64.b64encode(password.encode("utf-8")).decode("utf-8")
+
+
+def decode_password(password):
+    return base64.b64decode(password.encode("utf-8")).decode("utf-8")
+
+
+def init_config():
+    with open(config_path, "w") as f:
+        json.dump(
+            {i: encode_password(_ask(questions[i])) for i in questions},
+            f,
+            indent=4,
+            ensure_ascii=False,
+        )
+    QproDefaultConsole.print(QproInfoString, f'您的校园网账号和密码已加密保存在 "{config_path} 中!"')
+
+
+class CUPNetworkConfig:
+    def __init__(self):
+        if not os.path.exists(config_path):
+            init_config()
+        with open(config_path, "r") as f:
+            self.config = json.load(f)
+
+    def select(self, item):
+        if item not in self.config and item in questions:
+            res = _ask(questions[item])
+            self.update(item, encode_password(res) if item != "docker-url" else res)
+        return (
+            decode_password(self.config[item])
+            if item != "docker-url"
+            else self.config[item]
+        )
+
+    def update(self, key, value):
+        if not value or value == "None":
+            self.config.pop(key, None)
+        else:
+            self.config[key] = value
+        with open(config_path, "w") as f:
+            json.dump(self.config, f, indent=4, ensure_ascii=False)
---
+++ build/lib/CUP_Network/main.py
@@ -0,0 +1,108 @@
+from QuickProject.Commander import Commander
+from QuickProject import QproDefaultConsole
+
+app = Commander("cup-network")
+
+
+@app.command()
+def login():
+    """
+    利用 selenium 自动登录校园网
+    """
+    from . import login
+
+    with QproDefaultConsole.status("正在登录校园网..."):
+        login()
+
+
+@app.command()
+def logout():
+    """
+    利用 selenium 自动登出校园网
+    """
+    from . import logout
+
+    with QproDefaultConsole.status("正在登出校园网..."):
+        logout()
+
+
+@app.command()
+def reset():
+    """
+    重置校园网账号密码
+    """
+    from .__config__ import init_config
+
+    init_config()
+
+
+@app.command()
+def config(key: str, value: str = None):
+    """
+    操作配置表
+    """
+    from . import config
+
+    if value is None:
+        QproDefaultConsole.print(key, config.select(key))
+    else:
+        config.update(key, value)
+
+
+@app.command()
+def doc():
+    """
+    文档
+    """
+    QproDefaultConsole.print(
+        "[bold cyan][CUP_Network][/]", "CUP校园网命令工具", justify="center"
+    )
+    QproDefaultConsole.print("[bold]作者: RhythmLian[/]\n", justify="center")
+    QproDefaultConsole.print(
+        """\
+在无桌面系统中，推荐使用docker部署selenuim，再使用此工具。大致步骤如下:
+
+1. 安装docker
+2. 运行命令: "docker run -d -p <某个端口号>:4444 --shm-size=2g selenium/standalone-chrome"
+3. 初次运行 "cup-network" 会自动引导进行配置。
+   1. 命令: "cup-network config <key> \[value]" 可以修改配置。
+      对于 "value" 参数: 不填写表示查询, 设置为 [bold magenta]None[/] 表示删除此项。
+   2. 命令: "cup-network config docker-url http://<IP或域名>:<某个端口号>/wd/hub" 可设置使用docker selenium 
+      (1) 注意: <某个端口号> 为第二步中的端口号
+      (2) 注意: <IP或域名> 为服务器的IP或域名
+                服务器非本机的情况下，需要保证服务器与本机在同一局域网内。"""
+    )
+
+
+@app.command()
+def status():
+    """
+    查看校园网状态
+    """
+    from . import status
+
+    status()
+
+
+@app.command()
+def cron():
+    """
+    检查并保持登录状态
+    """
+    from . import login, status
+
+    flag = status()
+    if not flag:
+        login()
+
+
+def main():
+    """
+    注册为全局命令时, 默认采用main函数作为命令入口, 请勿将此函数用作它途.
+    When registering as a global command, default to main function as the command entry, do not use it as another way.
+    """
+    app()
+
+
+if __name__ == "__main__":
+    main()
